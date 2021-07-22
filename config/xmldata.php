5112d64e2910ca72eb7aa0cc5f3931f5088bc2b9
Yeyen Pamula <ypamula@gmail.com>
2021-07-22 11:31:54 +0700

    update path xml data

--- config/xmldata.php
+++ config/xmldata.php
@@ -1,6 +1,6 @@
 <?php
 
 return [
-    'breaking' => 'https://www.solopos.com/xml-data/breaking/',
-    'posts' =>  'https://www.solopos.com/xml-data/posts/',
+    'breaking' => 'https://cdn.solopos.com/xml-data/breaking/',
+    'posts' =>  'https://cdn.solopos.com/xml-data/posts/',
 ];
