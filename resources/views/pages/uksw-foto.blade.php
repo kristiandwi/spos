85ef8b9f6e39d38d24d9b88fbead48f3755aa593
sinichi <echo.elmob@gmail.com>
2021-08-04 11:28:53 +0700

    Update UKSW

--- resources/views/pages/uksw-foto.blade.php
+++ resources/views/pages/uksw-foto.blade.php
@@ -1,54 +1,54 @@
 @extends('layouts.app')
 @section('content')
-<div class="gap-30"></div>
-
 	<section class="main-content category-layout-1 pt-0">
 		<div class="container">
 			<div class="post-header-area" align="center">
 				<h2 class="post-title title-lg">Galeri Foto UKSW Salatiga</h2>
 			</div>	
-					<div class="row ts-gutter-30">
-					@php $f_loop = 1; @endphp
-                    @foreach($foto as $post)				
-						<div class="col-lg-6 col-md-6">
-							<div class="post-block-style post-overaly-style post-md" style="background-image:url({{ $post['images']['thumbnail'] }})">
-								<div class="featured-post">
-									<a href="{{ url("/{$post['slug']}-{$post['id']}") }}" title="{{ $post['title'] }}" class="image-link">&nbsp;</a>
-									<div class="overlay-post-content">
-										<div class="post-content">
-											<div class="grid-category">
-												<a class="post-cat lifestyle" href="https://www.solopos.com/uksw/foto">Galeri Foto</a>
-											</div>
-		
-											<h2 class="post-title title-md">
-												<a href="{{ url("/{$post['slug']}-{$post['id']}") }}" title="{{ $post['title'] }}">{{ $post['title'] }}></a>
-											</h2>
-											<div class="post-meta">
-												<ul>
-													<li><a href="#"><i class="fa fa-user"></i> @if($post['author']) {!! $post['author'] !!} @endif </a></li>
-													<li><a href="#"><i class="icon icon-clock"></i> {{ Helper::time_ago($post['date']) }} </a></li>
-												</ul>
-											</div>
-										</div>
+			
+			<div class="gap-50"></div>
+			
+			<div class="row ts-gutter-30">
+				@php $f_loop = 1; @endphp
+				@foreach($foto as $post)				
+				<div class="col-lg-6 col-md-6">
+					<div class="post-block-style post-overaly-style post-md" style="background-image:url({{ $post['images']['thumbnail'] }})">
+						<div class="featured-post">
+							<a href="{{ url("/{$post['slug']}-{$post['id']}") }}" title="{{ $post['title'] }}" class="image-link">&nbsp;</a>
+							<div class="overlay-post-content">
+								<div class="post-content">
+									<div class="grid-category">
+										<a class="post-cat lifestyle" href="https://www.solopos.com/uksw/foto">Galeri Foto</a>
 									</div>
-								</div><!--/ Featured post end -->
-							</div><!-- Post block a end -->
-						</div><!-- Col 1 end -->	
-						@php $f_loop++; @endphp @endforeach 																
-					</div><!-- row end -->
-					<div class="gap-30 d-none d-md-block"></div>
-					<div class="row">
-						<div class="col-12" align="center">
-							<ul class="ts-pagination">
-								<li><a href="#">1</a></li>
-								<li><a href="#" class="active">2</a></li>
-								<li><a href="#">3</a></li>
-								<li><a href="#"><i class="fa fa-angle-right"></i></a></li>
-							</ul>
-						</div>
-					</div>
-				</div><!-- col-lg-8 -->
+		
+									<h2 class="post-title title-md">
+										<a href="{{ url("/{$post['slug']}-{$post['id']}") }}" title="{{ $post['title'] }}">{{ $post['title'] }}></a>
+									</h2>
+									<div class="post-meta">
+										<ul>
+											<li><a href="#"><i class="fa fa-user"></i> @if($post['author']) {!! $post['author'] !!} @endif </a></li>
+											<li><a href="#"><i class="icon icon-clock"></i> {{ Helper::time_ago($post['date']) }} </a></li>
+										</ul>
+									</div>
+								</div>
+							</div>
+						</div><!--/ Featured post end -->
+					</div><!-- Post block a end -->
+				</div><!-- Col 1 end -->	
+				@php $f_loop++; @endphp @endforeach 																
 			</div><!-- row end -->
+			
+			<div class="gap-30 d-none d-md-block"></div>
+			<div class="row">
+				<div class="col-12" align="center">
+					<ul class="ts-pagination">
+						<li><a href="#">1</a></li>
+						<li><a href="#" class="active">2</a></li>
+						<li><a href="#">3</a></li>
+						<li><a href="#"><i class="fa fa-angle-right"></i></a></li>
+					</ul>
+				</div>
+			</div>
 		</div><!-- container end -->
 	</section><!-- category-layout end -->
 
--- resources/views/pages/uksw-kontak.blade.php
+++ resources/views/pages/uksw-kontak.blade.php
@@ -1,8 +1,39 @@
 @extends('layouts.app')
 @section('content')
-	<div class="gap-50"></div>
-	<section class="main-content category-layout-1 pt-0">
-				TEST PAGE KONTAK
-	</section>
+	<section class="main-content pt-0">
+		<div class="container">
+			<div class="row justify-content-center">
+				<div class="col-lg-8">
+					<img style="max-width: 100%; margin-bottom: 30px;" src="https://www.uksw.edu/public/upload/2019/11/30/USER120191130112455.jpg">
+					<h3>Kontak Kami</h3>
+					<p>Universitas Kristen Satya Wacana dapat mewujudkan citra dirinya seperti sekarang ini setelah melalui tahap-tahap perkembangan kelembagaan yang dalam garis besarnya adalah sebagai berikut: diujung tahun 1954 Ds Basoeki Probowinoto dan Ds Tan Ik Hay dan beberapa orang sebagai pendidik Kristen menggagas berdirinya suatu Perguruan Tinggi Pendidikan Guru Kristen. <a href="https://www.uksw.edu/pages/sejarah-uksw" target="_blank">Selengkapnya...</a></p>
+					<div class="widget contact-info">
+						<div class="contact-info-box">
+							<div class="contact-info-box-content">
+								<h4>UKSW Salatiga</h4>
+								<p>Jl. Diponegoro 52-60 Salatiga - Indonesia 50711</p>
+							</div>
+						</div>
 
+						<div class="contact-info-box">
+							<div class="contact-info-box-content">
+								<h4>Alamat Eamil</h4>
+								<p>humas@adm.uksw.edu</p>
+							</div>
+						</div>
+
+						<div class="contact-info-box">
+							<div class="contact-info-box-content">
+								<h4>Nomor Telepon</h4>
+								<p>+(0298) 321212</p>
+								<p>08112700049 (Admisi)</p>
+								<p>0811262080 (Humas)</p>
+							</div>
+						</div>
+
+					</div><!-- Widget end -->
+				</div><!-- col-lg-8 -->
+			</div><!-- row end -->
+		</div><!-- container end -->
+	</section><!-- category-layout end -->
 @endsection
